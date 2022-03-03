4d788a8a803862e613ee7cf516496fd15be6a1b7
Park YengSeo <1park4170@gmail.com>
2022-03-03 16:38:50 +0900

    filtering 2nd version

--- drinks/views.py
+++ drinks/views.py
@@ -1,60 +1,68 @@
 import json
 
-from django.http import JsonResponse
-from django.views import View
+from django.http     import JsonResponse
+from django.views   import View
 
 from drinks.models import Drink
 # Create your views here.
 
-class FilteringView(View):  # 최신순 , 가격순(range사용), 리뷰순 #처음에는 db에 들어있는대로 나열 
+class FilteringView(View):  
     def post(self, request):
-        # try:
-        data = json.loads(request.body)
+        data     = json.loads(request.body)
         method = data["method"]
 
         if method == "recent":
-            recently_ordered_list = Drink.objects.all().order_by('-updated_at') #생성시간이 아니라 업뎃시간 기준으로 만듬 
-            lst = []
-            for i in recently_ordered_list:
-                lst.append(i.name)
-            return JsonResponse({'message':lst}, status=200) #뭘 리턴해줘야 하는지, 객체냐 이름이냐 pk값이냐
+            recently_ordered_queryset = Drink.objects.all().order_by('-updated_at')
+            date_ordered_list = []
+            for i in recently_ordered_queryset:
+                date_ordered_list.append(i.name)
+            return JsonResponse({'message':date_ordered_list}, status=200) 
 
         elif method == "price":
-            price_range = data["price_range"] #프론트에서 리스트로 가격의 시작값과 끝값 보내줌
-            qualified_drinks = Drink.objects.filter(price__range=(price_range[0],price_range[1]+1))
+            price_range                  = data["price_range"] 
+            qualified_drinks             = Drink.objects.filter(price__range=(price_range[0],price_range[1]+1))
             qualified_drinks_in_order = qualified_drinks.order_by('price')
             lst = []
             for i in qualified_drinks_in_order:
                 lst.append(i.name)
             return JsonResponse({'message':lst}, status=200)
          
-            # drinks = Drink.objects.all()
-            # qualified_drinks = []
-            # for i in drinks:
-            #     if i.price in range(price_range[0],price_range[1]+1):
-            #         qualified_drinks.append(i.name)
-            # for j in qualified_drinks:
-
-        elif method == "reviews": # 리뷰 평점 높은 순 
-            # drink_count = Drink.objects.count()  # drinks 전체 갯수(평균낼때 사용)
+        elif method == "reviews": 
             drinks = Drink.objects.all()
             drink_and_average_rating = {}
             for drink in drinks:
-                drink_reviews = drink.review_set.all() # 특정 drink를 정참조하는 리뷰들 전체의 인스턴스  
-                review_count = drink_reviews.count() # 특정 drink의 리뷰 갯수
-                sum_rating = 0
-                for review in drink_reviews:        # drink에 해당하는 리뷰들 for문 돌림
+                drink_reviews = drink.review_set.all() 
+                review_count = drink_reviews.count() 
+                sum_rating    = 0
+                for review in drink_reviews:       
                     sum_rating += review.rating
-                if review_count == 0:               # division by zero 방지
+                if review_count == 0:              
                     drink_average_review = 0
-                elif review_count != 0:                 # 0으로 나눠주는 경우 아닐 시 
+                elif review_count != 0:                 
                     drink_average_review = sum_rating / review_count
                 drink_and_average_rating[drink.name] = drink_average_review
-            sorted_dict = sorted(drink_and_average_rating.items(), key=lambda x: x[1], reverse=True)
+            sorted_dict = sorted(drink_and_average_rating.items(), key=lambda x: x[1], reverse=True) #(키,값) 이렇게 정렬
             sorted_key_list= []
             for i in sorted_dict:
                 sorted_key_list.append(i[0])
 
             return JsonResponse({'message':sorted_key_list}, status=200)
+
+        elif method == "caffein":
+            if data["caffeine"] == "caffeinated":
+                drinks = Drink.objects.all()
+                caffein_drinks = drinks.filter(caffeine__range =(1,10000)).order_by('caffeine')
+                caffein_drinks_list = []
+                for drink in caffein_drinks:
+                    caffein_drinks_list.append(drink.name)
+                return JsonResponse({'message':caffein_drinks_list}, status=200)
+            
+            elif data["caffeine"] == "decaffeinated":
+                drinks = Drink.objects.all()
+                decaffein_drinks = drinks.filter(caffeine = 0)
+                decaffein_drinks_list = []
+                for drink in decaffein_drinks:
+                    decaffein_drinks_list.append(drink.name)
+                return JsonResponse({'message':decaffein_drinks_list}, status=200)
 
 
