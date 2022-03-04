7462ca299d38132723cbc4754a4347ba546fd2a7
Park YengSeo <1park4170@gmail.com>
2022-03-04 09:10:26 +0900

    filtering in progress Json completed

--- drinks/views.py
+++ drinks/views.py
@@ -83,51 +83,67 @@ class FilteringView(View):
                     drink_average_review = sum_rating / review_count
                 drink_and_average_rating[drink.name] = drink_average_review
             sorted_dict = sorted(drink_and_average_rating.items(), key=lambda x: x[1], reverse=True) 
+
             sorted_key_list= []
             for i in sorted_dict:
                 sorted_key_list.append(i[0])
-
-   # rating순으로 name 정렬까진 됐는데, 이 정렬된 아이들에 해당하는 instance를 통해서 name,price,평점평균,평점갯수를 딕셔너리에 담아서 리턴해줘야 함 
-
-    
-            # data_ordered_list = [] 
-            # for i in sorted_key_list:
-            #     data_dict = {}
-            #     drink_inorder = drinks.get(name=i)
-            #     data_dict["name"] = drink_inorder.name
-            #     data_dict["price"] = drink_inorder.price
-            #     drink_reviews = drink.review_set.all() 
-            #     review_count  = drink_reviews.count() 
-            #     sum_rating    = 0
-            #     for review in drink_reviews:       
-            #         sum_rating += review.rating
-            #     if review_count == 0:              
-            #         drink_average_review = 0
-            #     elif review_count != 0:                 
-            #         drink_average_review = sum_rating / review_count
-            #     data_dict["average_rating"] = drink_inorder.drink_average_review
-            #     data_dict["review_count"] = drink_inorder.reveiw.count
-            #     data_ordered_list.append(data_dict)
-            # return JsonResponse({'message':data_ordered_list}, status=200)
+            data_ordered_list = []
+            for name in sorted_key_list:
+                data_dict = {}
+                drink = Drink.objects.get(name=name)
+                data_dict["name"] = drink.name
+                data_dict["price"] = drink.price
+                data_dict["average_rating"] = drink_and_average_rating[name]
+                data_dict["review_count"] = drink.review_set.all().count()
+                data_ordered_list.append(data_dict)
+            return JsonResponse({'message':data_ordered_list}, status=200) 
 
 
 
 
-        elif method == "caffein":
+        elif method == "caffeine":
             if data["caffeine"] == "caffeinated":
                 drinks = Drink.objects.all()
                 caffein_drinks  = drinks.filter(caffeine__range =(1,10000)).order_by('caffeine')
-                caffein_drinks_list = []
+                data_ordered_list = []
                 for drink in caffein_drinks:
-                    caffein_drinks_list.append(drink.name)
-                return JsonResponse({'message':caffein_drinks_list}, status=200)
+                    data_dict = {}
+                    data_dict["name"] = drink.name
+                    data_dict["price"]  = drink.price
+                    drink_reviews = drink.review_set.all() 
+                    review_count  = drink_reviews.count() 
+                    sum_rating    = 0
+                    for review in drink_reviews:       
+                        sum_rating += review.rating
+                    if review_count == 0:              
+                        drink_average_review = 0
+                    elif review_count != 0:                 
+                        drink_average_review = sum_rating / review_count
+                    data_dict["average_rating"] = drink_average_review
+                    data_dict["review_count"] = review_count
+                    data_ordered_list.append(data_dict)
+                return JsonResponse({'message':data_ordered_list}, status=200)
             
             elif data["caffeine"] == "decaffeinated":
                 drinks = Drink.objects.all()
                 decaffein_drinks    = drinks.filter(caffeine = 0)
-                decaffein_drinks_list = []
+                data_ordered_list = []
                 for drink in decaffein_drinks:
-                    decaffein_drinks_list.append(drink.name)
-                return JsonResponse({'message':decaffein_drinks_list}, status=200)
+                    data_dict = {}
+                    data_dict["name"] = drink.name
+                    data_dict["price"]  = drink.price
+                    drink_reviews = drink.review_set.all() 
+                    review_count  = drink_reviews.count() 
+                    sum_rating    = 0
+                    for review in drink_reviews:       
+                        sum_rating += review.rating
+                    if review_count == 0:              
+                        drink_average_review = 0
+                    elif review_count != 0:                 
+                        drink_average_review = sum_rating / review_count
+                    data_dict["average_rating"] = drink_average_review
+                    data_dict["review_count"] = review_count
+                    data_ordered_list.append(data_dict)
+                return JsonResponse({'message':data_ordered_list}, status=200)
 
 
