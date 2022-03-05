4144a865830953c5548c95be6c6681d8909bc53c
Park YengSeo <1park4170@gmail.com>
2022-03-05 19:23:05 +0900

    Q works

--- drinks/views.py
+++ drinks/views.py
@@ -1,8 +1,10 @@
 from itertools import product
 import json
 from nis import cat
+import re
 from this import d
 from unicodedata import category
+from unittest.util import sorted_list_difference
 
 from django.http     import JsonResponse
 from django.views   import View
@@ -31,26 +33,74 @@ class ProductsView(View):
 
         q = Q()
 
-        category = request.GET.get("category", None)  
+        category = request.GET.get("category", None)   # 카테고리 여러개 올 수 있음  # GET으로 받고 split으로 나눠줘야함
         caffeine = request.GET.get("caffeine", None)
-        price = request.GET.getlist("price", None)   #리스트 형태로 값이 들어옴 . 따라서 filter 적용시 __in 써줘야 함
-        
+        price_upper = request.GET.get("price_upper", 300000)  # 원래 getlist 썼다가, 각 인자로 range만들때 index error 떠서 얘로 바꿈 
+        price_lower = request.GET.get("price_lower", 0) 
 
+      
         if category:
-            q.add(Q(category__name= category), Q.AND)   # __exact는 정확히 # 왜 id값만 받는지 나는 이름으로 필터링 하고 싶은데  # __name 으로 바꿔주니 되긴됨
+            categories = category.split(',')
+            q.add(Q(category__name__in = categories), Q.AND)   # __exact는 정확히 # 왜 id값만 받는지 나는 이름으로 필터링 하고 싶은데  # __name 으로 바꿔주니 되긴됨
+           
 
         if caffeine:
-            if caffeine > 0:
+            if caffeine == "yes":
                 q.add(Q(caffeine__gt=0), Q.AND)  #__gt 는 보다 큼 
-            elif caffeine == 0:
+            elif caffeine == "no":
                 q.add(Q(caffeine__exact=0), Q.AND) # id 말고 다른 것으로 
         
-        filtered_drinks = drinks.filter(q)
-        lst = []
-        for i in filtered_drinks:
-            lst.append(i.name)
+        q.add(Q(price__range = (price_lower, price_upper)),Q.AND)
 
-        return JsonResponse({'message':lst}, status = 200)
+        filtered_drinks = drinks.filter(q) # 필터링까지 완료! 얘네를 가지고 오더링 해서 리턴해줘야 함 
+
+
+        recently = request.GET.get("recently", None)
+        rating = request.GET.get("rating", None)     # rating과 recently가 한번에 올 수 는 없음 
+        
+        if recently:
+            filtered_drinks = filtered_drinks.order_by('-updated_at')
+            drink_and_average_rating = {}
+            for drink in filtered_drinks:      # 미리 리뷰 갯수와 평점평균 계산해줌(json에 넣어야 하니깐)
+                review_count , drink_average_review = compute_reviews(drink)
+                drink_and_average_rating[drink.name] = drink_average_review
+            whole_data_list = []
+            for drink in filtered_drinks:
+                data_dict={}
+                data_dict["name"] = drink.name
+                data_dict["price"] = drink.price
+                data_dict["average_rating"] = drink_average_review
+                data_dict["review_count"] = review_count
+                whole_data_list.append(data_dict)
+
+
+        elif rating:
+            drink_and_average_rating = {}
+            for drink in filtered_drinks:      # 미리 리뷰 갯수와 평점평균 계산해줌(json에 넣어야 하니깐)
+                review_count , drink_average_review = compute_reviews(drink)
+                drink_and_average_rating[drink.name] = drink_average_review
+            sorted_dict = sorted(drink_and_average_rating.items(), key=lambda x: x[1], reverse=True)
+            sorted_key_list = []
+            for i in sorted_dict:
+                sorted_key_list.append(i[0])   # 평균 평점 높은 순으로 키값 정렬해서 sorted_key_list에 넣음 
+            whole_data_list = []
+            for name in sorted_key_list:
+                data_dict = {}
+                drink = Drink.objects.get(name=name)
+                data_dict["name"] = drink.name
+                data_dict["price"] = drink.price
+                data_dict["average_rating"] = drink_and_average_rating[name]
+                data_dict["review_count"] = drink.review_set.all().count()
+                whole_data_list.append(data_dict)
+
+        else:  #정렬 요청 아무것도 없는 경우
+            pass     
+
+        return JsonResponse({'message':whole_data_list}, status = 200)
+
+        # lst = []
+        # for i in filtered_drinks:
+        #     lst.append(i.name)
 
         # if min_price and max_price:
         #     q &= Q()
