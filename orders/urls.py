78c04b4a232e43d3f53dcdcfb7784540e4cd5a5c
ahn <sungjoonan02@gmail.com>
2022-03-08 18:27:25 +0900

    Cart url path 수정3

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from orders.views import CartView
 
 urlpatterns = [
-    path('/cart/drink-id=<int:drink_id>/cart-id=<int:cart_id>', CartView.as_view())
+    path('/cart/<int:cart_id>/drink/<int:drink_id>', CartView.as_view())
 ]
