7f0a911abadd9c9878d049e8d7e818e46ed491d1
Park YengSeo <1park4170@gmail.com>
2022-03-09 13:37:00 +0900

    commit for checkout this branch should not be pushed

--- drinks/views.py
+++ drinks/views.py
@@ -2,14 +2,51 @@ from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q, Avg, Count
 
-
 from drinks.models import Drink, Farm
+
+#검색 기능 위한 전역변수
+q_for_search = None
+
+
+#ㅇㅇ        
+
+
+class FarmProductsView(View):
+    def get(self, request):
+
+
+        #여기부터
+        search_query    = request.GET.get('search_query', None)
         
-      
+        if search_query:
+            global q_for_search
+            q_for_search = Q(name__contains=search_query), Q.AND
+
+
+        #여기까지는 검색기능 시도 
+        
+        farms = Farm.objects.all()
+
+        farms_name_dic = {farm.name: [{
+
+            "name"           : drink.name,
+            "price"          : drink.price,
+            "average_rating" : drink.average_rating,
+            "review_count"   : drink.review_count,
+            "image"          : drink.drinkimage_set.all()[0].thumb_img 
+
+        }for drink in farm.drink_set.all().annotate(average_rating = Avg('review__rating'), review_count=Count('review')).order_by('-average_rating')[:4]] for farm in farms}
+
+        return JsonResponse({'result':farms_name_dic}, status = 200)
+
 
 class ProductsView(View):
     def get(self, request):
-        
+        #다른페이지검색기능
+
+
+
+        #ㅇㅇㅇㅇㅇ
         q = Q()
 
         category        = request.GET.getlist("category", None)  
@@ -19,7 +56,10 @@ class ProductsView(View):
         search_query    = request.GET.get('search_query', None) 
 
         if search_query:
-            q.add(Q(name__contains=search_query), Q.AND)
+            global q_for_search
+            q.add(q_for_search)
+
+            #q.add(Q(name__contains=search_query), Q.AND)
         
         if category:
             categories = category[0].split(',')
@@ -50,20 +90,3 @@ class ProductsView(View):
 
         return JsonResponse({'result':result}, status = 200)
 
-
-class FarmProductsView(View):
-    def get(self, request):
-        
-        farms = Farm.objects.all()
-
-        farms_name_dic = {farm.name: [{
-
-            "name"           : drink.name,
-            "price"          : drink.price,
-            "average_rating" : drink.average_rating,
-            "review_count"   : drink.review_count,
-            "image"          : drink.drinkimage_set.all()[0].thumb_img 
-
-        }for drink in farm.drink_set.all().annotate(average_rating = Avg('review__rating'), review_count=Count('review')).order_by('-average_rating')[:4]] for farm in farms}
-
-        return JsonResponse({'result':farms_name_dic}, status = 200)
