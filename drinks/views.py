4ca678865cfa72e0e56187f046009bfeb406572c
Park YengSeo <1park4170@gmail.com>
2022-03-10 10:00:40 +0900

    form changed

--- drinks/views.py
+++ drinks/views.py
@@ -57,14 +57,24 @@ class FarmProductView(View):
         offset = request.GET.get("offset", 0)
         limit = request.GET.get("limit", 4)
 
-        farms_name_dic = {farm.name: [{
-            "id"             : drink.id,
-            "name"           : drink.name,
-            "price"          : drink.price,
-            "average_rating" : drink.average_rating,
-            "review_count"   : drink.review_count,
-            "image"          : drink.drinkimage_set.all().first().thumb_img 
+        result = {
+                farm.name: [{
+                    "farm_id"  : farm.id,
+                    "farm_name" : farm.name,
+                    "drink" : {
+                        "id"             : drink.id,
+                        "name"           : drink.name,
+                        "price"          : drink.price,
+                        "average_rating" : drink.average_rating,
+                        "review_count"   : drink.review_count,
+                        "image"          : drink.drinkimage_set.all().first().thumb_img 
+                    }        
 
-        }for drink in farm.drink_set.all().annotate(average_rating = Avg('review__rating'), review_count=Count('review')).order_by('-average_rating')[offset:offset+limit]] for farm in farms}
+            }for drink in farm.drink_set.all().annotate(average_rating = Avg('review__rating'), review_count=Count('review')).order_by('-average_rating')[offset:offset+limit]] for farm in farms}
 
-        return JsonResponse({'result':farms_name_dic}, status = 200)
+
+        return JsonResponse({'result':result}, status = 200)
+
+
+  
+  
