13e821a19dddcf2f22b15df8cec1fd21bf740e6f
Park YengSeo <1park4170@gmail.com>
2022-03-10 14:47:33 +0900

    conventionn2

--- drinks/views.py
+++ drinks/views.py
@@ -66,7 +66,7 @@ class FarmProductView(View):
                     "drinks" : [
                         {
                             "id"             : drink.id,
-                            "name"          : drink.name,
+                            "name"           : drink.name,
                             "price"          : drink.price,
                             "average_rating" : drink.average_rating,
                             "review_count"   : drink.review_count,
